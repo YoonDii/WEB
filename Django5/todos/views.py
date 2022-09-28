38596a8c7c06e0417a1c00f2c6159357e4e5983f
YoonDii <dbswl9506@naver.com>
2022-09-28 20:42:49 +0900

    0928

---
+++ Django5/todos/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+# Create your views here.
+
+
+def index(request):
+    _todos = Todo.objects.all()
+    context = {
+        "todos": _todos,
+    }
+    return render(request, "todos/index.html", context)
+
+
+def create(request):
+    content = request.GET.get("content___")
+    Todo.objects.create(content=content)
+
+    return redirect("todos:index")
+
+
+def delete(request, todo_pk):
+    todo = Todo.objects.get(pk=todo_pk)
+    todo.delete()
+
+    return redirect("todos:index")
