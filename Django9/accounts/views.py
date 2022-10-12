4a1cfc7be81155603fa716116c55a873e4fd6857
YoonDii <dbswl9506@naver.com>
2022-10-12 16:09:00 +0900

    logout add

--- Django9/accounts/views.py
+++ Django9/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import UsersForm
-
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from .models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
@@ -14,12 +15,12 @@ def home(request):
 
 def signup(request):
     if request.method == "POST":
-        form = UsersForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("accounts:home")
     else:
-        form = UsersForm()
+        form = CustomUserCreationForm()
     context = {"form": form}
     return render(request, "accounts/signup.html", context)
 
@@ -31,6 +32,22 @@ def detail(request, pk):
 
 
 def login(request):
-    form = AuthenticationForm()
+    if request.method == "POST":
+        # AuthenticationForm은 ModelForm이 아님!
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # 세션 저장
+            # login함수는 request,uer 객체를 인자로 받음
+            # user 객체는 form에서 인증된 유저정보를 받을 수 있음
+            auth_login(request, form.get_user())
+            # form.save()
+            return redirect(request.GET.get("next") or "accounts:home")
+    else:
+        form = AuthenticationForm()
     context = {"form": form}
     return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:home")
