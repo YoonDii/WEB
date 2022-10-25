aac714bf7ae40db7073ad5dac47229dc5b2d50c3
YoonDii <dbswl9506@naver.com>
2022-10-25 16:27:47 +0900

    add follow

--- Django15/accounts/views.py
+++ Django15/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # from .models import User
 from django.contrib import messages
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_POST
 from django.contrib.auth.forms import AuthenticationForm
 
 # from django.contrib.auth.forms import UserCreationForm
@@ -66,3 +67,18 @@ def update(request):
         form = CustomUserChangeForm(instance=request.user)
     context = {"form": form}
     return render(request, "accounts/update.html", context)
+
+
+@require_POST
+def follow(request, pk):
+    if request.user.is_authenticated:
+        user = get_user_model()
+        person = user.objects.get(pk=pk)
+        if person != request.user:
+            if person.followers.filter(pk=request.user.pk).exists():
+                # if request.user in person.followers.all():
+                person.followers.remove(request.user)
+            else:
+                person.followers.add(request.user)
+        return redirect("accounts:detail", person.username)
+    return redirect("accounts:login")
--- Django15/articles/views.py
+++ Django15/articles/views.py
@@ -93,17 +93,13 @@ def comment_create(request, pk):
         comment.save()
     return redirect("articles:detail", article.pk)
 
+
 @login_required
-def like(request,pk):
+def like(request, pk):
     article = Article.objects.get(pk=pk)
-    # 만약에 로그인한 유저가 이 글을 좋아요를 눌렀다면,
-    # if articles.like_users.filter(id=request.id).exists():
-    if request.user in article.like_users.all():
-        #좋아요 삭제하고
+    if article.like_users.filter(pk=request.user.pk).exists():
+        # if request.user in article.like_users.all():
         article.like_users.remove(request.user)
     else:
-        #좋아요 추가하고
         article.like_users.add(request.user)
-    #상세페이지로 redirect
-    return redirect('articles:detail',pk)
-
+    return redirect("articles:index")
